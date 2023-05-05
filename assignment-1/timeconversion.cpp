91820e29fa5df3243c137535f805ed5c4c6fd264
Anusha Kabasi <115303715+anushakabasii@users.noreply.github.com>
2023-05-05 22:19:49 +0530

    Create timeconversion.cpp

---
+++ assignment-1/timeconversion.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char a[10];
+    int i;
+    for (i=0;i<10;i++)
+    {
+        cin>>a[i];
+    }
+    
+    if(a[8]=='A'&& a[0]=='1'&&a[1]=='2')
+    {
+        a[0]='0';
+        a[1]='0';
+    }
+  
+    if (a[8]=='P')
+    
+    {
+        if (a[0]=='1'&&a[1]=='2')
+        {
+            a[0]='1';
+            a[1]='2';
+        }
+        else
+        {
+        a[0]=a[0]+1;
+        a[1]=a[1]+2;
+        }
+    }
+     
+    for (i=0;i<8;i++)
+    {
+        cout<<a[i];
+    }
+    return 0;
+}

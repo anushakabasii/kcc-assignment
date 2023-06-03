3aded76fcce2861fc1e32f0b5da1049be812c3d2
Anusha Kabasi <115303715+anushakabasii@users.noreply.github.com>
2023-06-03 10:16:11 +0530

    Create matrixaddition.cpp

---
+++ assignment4/matrixaddition.cpp
@@ -0,0 +1,25 @@
+int main() {
+    int n;
+    cin>>n;
+    int a[n][n],b[n][n];
+    for(int r=0;r<n;r++){
+        for(int c=0;c<n;c++){
+            cin>>a[r][c];
+            cin>>b[r][c];
+        }
+    }
+     for(int r=0;r<n;r++){
+        for(int c=0;c<n;c++){
+            cin>>b[r][c];
+        }
+    }
+
+    
+    for(int r=0;r<n;r++){
+        for(int c=0;c<n;c++){
+            cout<<a[r][c]+b[r][c];
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
